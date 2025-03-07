ee5b71914ae45ad93426c5454d290139163e8af5
abc <>
2025-03-07 22:52:35 +0900

    Add Color extension

---
+++ Projects/UI/Sources/Extension/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  UI
+//
+//  Created by abc on 3/7/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import SwiftUICore
+
+public extension Color {
+    static let primaryColor = Color("Primary", bundle: Bundle.module)
+    static let secondaryColor = Color("Secondary", bundle: Bundle.module)
+    
+    // Bottom Button
+    static let pressedButtonBackground = Color("PressedButtonBackground", bundle: Bundle.module)
+    static let disabledButtonBackground = Color("DisabledButtonBackground", bundle: Bundle.module)
+    static let disabledButtonText = Color("DisabledButtonText", bundle: Bundle.module)
+}
