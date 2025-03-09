5fbcdd0aef99dcb14ecfe7f7dc5e150d8d44570e
delight010 <osa070@knou.ac.kr>
2025-03-10 00:03:07 +0900

    Create PlanBuilderViewModel

---
+++ Projects/Features/Sources/ViewModel/PlanBuilderViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  PlanBuilderViewModel.swift
+//  Features
+//
+//  Created by abc on 3/9/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Core
+
+import Combine
+import SwiftUI
+
+protocol PlanBuilderViewModelProtocol {
+    func setDateRange(startDate: Date, endDate: Date)
+    func setCurrency(_ currency: Currency)
+    func setBudget(_ budget: Decimal)
+}
+
+class PlanBuilderViewModel: ObservableObject, PlanBuilderViewModelProtocol {
+    @Published private var plan: Plan?
+    
+    init() {}
+    
+    func setDateRange(startDate: Date, endDate: Date) {
+        plan?.startDate = startDate
+        plan?.endDate = endDate
+    }
+    
+    func setCurrency(_ currency: Core.Currency) {
+        plan?.currency = currency
+    }
+    
+    func setBudget(_ budget: Decimal) {
+        plan?.budget = budget
+    }
+}
