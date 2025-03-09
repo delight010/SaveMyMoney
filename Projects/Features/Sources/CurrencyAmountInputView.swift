b4c603c901cf864c18e40afc92dcd86b969611a8
delight010 <osa070@knou.ac.kr>
2025-03-09 14:13:39 +0900

    Create CurrencyAmountInputView

---
+++ Projects/Features/Sources/CurrencyAmountInputView.swift
@@ -0,0 +1,81 @@
+//
+//  CurrencyAmountInputView.swift
+//  Features
+//
+//  Created by abc on 3/8/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Core
+import UI
+import SwiftUI
+
+public struct CurrencyAmountInputView: View {
+    @Environment(\.locale) var locale
+    @State private var isPositive: Bool = false
+    @State private var selectedCurrency: Currency = Currency.currencies[0]
+    @State private var amount: Decimal = 0
+    private let progress = 0.66
+    
+    public init() {
+        let identifier = locale.currency?.identifier ?? "en_US"
+        selectedCurrency = Currency.currenciesDictionary[identifier] ?? Currency.currencies[0]
+    }
+    
+    public var body: some View {
+        VStack {
+            ProgressView(value: progress)
+                .tint(.progressBarColor)
+                .padding()
+            
+            Text("Select currency and enter amount")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.bottom)
+            
+            VStack(alignment: .leading) {
+                Text("Currency")
+                    .bold()
+                Picker("", selection: $selectedCurrency) {
+                    ForEach(Currency.currencies) { currency in
+                        Text("\(currency.flag) \(currency.code)").tag(currency)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .pickerStyle(.menu)
+                .tint(.primary)
+                .labelsHidden()
+                .textRoundedRectangle()
+                
+                Text("Amount")
+                    .bold()
+                    .padding(.top, 5)
+                AmountTextField("\(selectedCurrency.currencySymbol) 0", value: $amount, currency: $selectedCurrency)
+                    .id(selectedCurrency)
+                
+                Text("Please enter a positive amount only.")
+                    .foregroundStyle(.secondary)
+            } // VStack
+            .onChange(of: selectedCurrency, { oldValue, newValue in
+                print(newValue)
+            })
+            .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("Next")
+            }
+            .buttonStyle(BottomButtonStyle(isPositive))
+        } // VStack
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 20)
+        .onChange(of: amount) { _, newValue in
+            isPositive = newValue > 0 ? true : false
+        }
+    }
+}
+
+#Preview {
+    CurrencyAmountInputView()
+}
