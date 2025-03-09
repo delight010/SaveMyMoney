4998431c9633991678dae0a508f9a83cab92ddad
delight010 <osa070@knou.ac.kr>
2025-03-09 23:13:03 +0900

    Add Expense Model

---
+++ Projects/Core/Sources/Model/Expense.swift
@@ -0,0 +1,25 @@
+//
+//  Expense.swift
+//  Core
+//
+//  Created by abc on 3/9/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+public final class Expense {
+    @Attribute(.unique) public var id: UUID
+    public var date: Date
+    public var amount: Decimal
+    public var tag: String
+    
+    public init(date: Date, amount: Decimal, tag: String) {
+        self.id = .init()
+        self.date = date
+        self.amount = amount
+        self.tag = tag
+    }
+}
