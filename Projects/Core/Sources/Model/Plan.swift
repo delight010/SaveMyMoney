ee727ad74de8f9b42c52e0fd81a5bf37e8ee2611
delight010 <osa070@knou.ac.kr>
2025-03-10 00:26:29 +0900

    Remove currency property from Plan model

--- Projects/Core/Sources/Model/Plan.swift
+++ Projects/Core/Sources/Model/Plan.swift
@@ -14,16 +14,14 @@ public final class Plan {
     @Attribute(.unique) public var id: UUID
     public var startDate: Date
     public var endDate: Date
-    public var currency: Currency
     public var budget: Decimal
     @Relationship(deleteRule: .cascade)
     public var consumption: [Expense]
     
-    public init(startDate: Date, endDate: Date, currency: Currency, budget: Decimal, consumption: [Expense]) {
+    public init(startDate: Date, endDate: Date, budget: Decimal, consumption: [Expense]) {
         self.id = .init()
         self.startDate = startDate
         self.endDate = endDate
-        self.currency = currency
         self.budget = budget
         self.consumption = consumption
     }
