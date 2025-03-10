78997f2e1ad745bb540a7714f431a9b6e30b74c1
delight010 <osa070@knou.ac.kr>
2025-03-11 07:53:21 +0900

    Create AppCoordinator

---
+++ Projects/SaveMyMoney/Sources/Coordinator/AppCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  AppCoordinator.swift
+//  SaveMyMoney
+//
+//  Created by abc on 3/11/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public protocol AppCoordinator {
+    var appRouter: AppRouter { get }
+    
+    func push(to destination: any Hashable)
+    func pop()
+    func popToRoot()
+}
+
+public extension AppCoordinator {
+    
+    func push(to destination: any Hashable) {
+        appRouter.push(to: destination)
+    }
+    
+    func pop() {
+        appRouter.pop()
+    }
+    
+    func popToRoot() {
+        appRouter.popToRoot()
+    }
+}
