7d568542b2743840a741564ebde9268e6ed339c7
delight010 <osa070@knou.ac.kr>
2025-03-11 07:50:46 +0900

    Create AppRouter

---
+++ Projects/SaveMyMoney/Sources/Coordinator/AppRouter.swift
@@ -0,0 +1,33 @@
+//
+//  AppRouter.swift
+//  SaveMyMoney
+//
+//  Created by abc on 3/11/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+final public class AppRouter: ObservableObject {
+    
+    @Published public var path = NavigationPath()
+    public var lastPath: AnyHashable?
+    
+    public init() {
+        self.path = path
+    }
+    
+    public func push(to destination: any Hashable) {
+        path.append(destination)
+    }
+    
+    public func pop(_ page: Int = 1) {
+        path.removeLast(page)
+    }
+    
+    public func popToRoot() {
+        path.removeLast(path.count)
+    }
+}
