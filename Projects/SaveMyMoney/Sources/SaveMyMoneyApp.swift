d7c8e5cb43d34985f1d3a1e43ae9086b4bf669e8
delight010 <osa070@knou.ac.kr>
2025-03-11 08:33:05 +0900

    Add @StateObject AppRouter property to App

--- Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
+++ Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
@@ -1,10 +1,15 @@
+import Features
+
 import SwiftUI
 
 @main
 struct SaveMyMoneyApp: App {
+    @StateObject private var router = AppRouter()
+    
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environmentObject(router)
         }
     }
 }
