c2298dce2703765160547100a15ee86b79e8c6e9
delight010 <osa070@knou.ac.kr>
2025-03-11 08:25:33 +0900

    Create RootCoordinator using AppCoordinator

---
+++ Projects/SaveMyMoney/Sources/RootCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  RootCoordinator.swift
+//  SaveMyMoney
+//
+//  Created by abc on 3/11/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Features
+
+import Foundation
+
+class RootCoordinator: AppCoordinator {
+    enum RootDestination: Hashable {
+        case home
+        case history
+        case setting
+    }
+    
+    var appRouter: AppRouter
+    
+    init(_ appRouter: AppRouter) {
+        self.appRouter = appRouter
+    }
+}
