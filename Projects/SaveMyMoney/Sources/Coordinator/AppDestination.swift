84d7e89035fb53d33e3c72bf8e1d1c6009e7674a
delight010 <osa070@knou.ac.kr>
2025-03-10 23:25:33 +0900

    Create AppDestination

---
+++ Projects/SaveMyMoney/Sources/Coordinator/AppDestination.swift
@@ -0,0 +1,13 @@
+//
+//  AppDestination.swift
+//  SaveMyMoney
+//
+//  Created by abc on 3/10/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+enum AppDestination {
+    case main
+    case history
+    case setting
+}
