cc78e3888169005d84b62ee4e733e31960e5315b
delight010 <osa070@knou.ac.kr>
2025-03-11 21:30:02 +0900

    Create PlanCoordinator

---
+++ Projects/Features/Sources/Coordinator/PlanCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  PlanCoordinator.swift
+//  Features
+//
+//  Created by abc on 3/11/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+
+import Foundation
+
+public class PlanCoordinator: AppCoordinator {
+    enum PlanDestination: Hashable {
+        case empty
+        case dateRange
+        case currency
+        case currencyAmount
+        case confirmation
+    }
+    
+    public var appRouter: AppRouter
+    
+    public init(_ appRouter: AppRouter) {
+        self.appRouter = appRouter
+    }
+}
