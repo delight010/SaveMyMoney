6020bbd316ab0dedfb7d1ed5d5e967243e0e4d77
delight010 <osa070@knou.ac.kr>
2025-03-11 20:55:14 +0900

    Create EmptyPlanView

---
+++ Projects/Features/Sources/Home/View/EmptyPlanView.swift
@@ -0,0 +1,36 @@
+//
+//  EmptyPlanView.swift
+//  Features
+//
+//  Created by abc on 3/11/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import UI
+
+import SwiftUI
+
+public struct EmptyPlanView: View {
+    @EnvironmentObject var router: AppRouter
+    
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Text("There is no plan currently in progress.")
+            Text("Would you like to start a new plan?")
+            Button {
+                
+            } label: {
+                Text("Create Plan")
+            }
+            .buttonStyle(CapsuleButtonStyle())
+        } // VStack
+    }
+}
+
+#Preview {
+    @Previewable @StateObject var router = AppRouter()
+    EmptyPlanView()
+        .environmentObject(router)
+}
