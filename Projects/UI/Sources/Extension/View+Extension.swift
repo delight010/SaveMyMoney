586840fc32c689c6427ca3ea9adb127782bd35b2
delight010 <osa070@knou.ac.kr>
2025-03-12 20:30:41 +0900

    Create backNavagationToolbar modifier in View extension

--- Projects/UI/Sources/Extension/View+Extension.swift
+++ Projects/UI/Sources/Extension/View+Extension.swift
@@ -49,24 +49,28 @@ public extension View {
     }
     
     func basicAlert(
-            isPresented: Binding<Bool>,
-            title: String,
-            message: String? = nil,
-            primaryButtonTitle: String,
-            secondaryButtonTitle: String? = nil,
-            primaryButtonAction: ( () -> Void)? = nil,
-            secondaryButtonAction: ( () -> Void)? = nil
-        ) -> some View {
-            let alert = BasicAlertView(
-                isPresented: isPresented,
-                title: title,
-                message: message,
-                primaryButtonTitle: primaryButtonTitle,
-                secondaryButtonTitle: secondaryButtonTitle,
-                primaryButtonAction: primaryButtonAction,
-                secondaryButtonAction: secondaryButtonAction
-            )
-            
-            return modifier(BasicAlertModifier(isPresented: isPresented, alert: alert))
-        }
+        isPresented: Binding<Bool>,
+        title: String,
+        message: String? = nil,
+        primaryButtonTitle: String,
+        secondaryButtonTitle: String? = nil,
+        primaryButtonAction: ( () -> Void)? = nil,
+        secondaryButtonAction: ( () -> Void)? = nil
+    ) -> some View {
+        let alert = BasicAlertView(
+            isPresented: isPresented,
+            title: title,
+            message: message,
+            primaryButtonTitle: primaryButtonTitle,
+            secondaryButtonTitle: secondaryButtonTitle,
+            primaryButtonAction: primaryButtonAction,
+            secondaryButtonAction: secondaryButtonAction
+        )
+        
+        return modifier(BasicAlertModifier(isPresented: isPresented, alert: alert))
+    }
+    
+    func backNavagationToolbar(action: @escaping () -> Void) -> some View {
+        return self.modifier(BackNavigationToolbarModifier(action: action))
+    }
 }
