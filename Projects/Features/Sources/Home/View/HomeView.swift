9873987d45acfd83f4bbd76dd341a327dc269051
delight010 <osa070@knou.ac.kr>
2025-03-12 21:43:10 +0900

    Add .ignoresSafeArea modifier to HomeView

--- Projects/Features/Sources/Home/View/HomeView.swift
+++ Projects/Features/Sources/Home/View/HomeView.swift
@@ -51,6 +51,7 @@ public struct HomeView: View {
                     PlanConfirmationView(viewModel: viewModel)
                 }
             }
+            .ignoresSafeArea(.keyboard)
         } // NavigationStack
     }
 }
