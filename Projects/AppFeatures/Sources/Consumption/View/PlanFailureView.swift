7d0e24e03b0c8a45a722f797eb40a269afb0acad
abc <>
2025-03-18 17:56:37 +0900

    Create PlanFailureView

---
+++ Projects/AppFeatures/Sources/Consumption/View/PlanFailureView.swift
@@ -0,0 +1,68 @@
+//
+//  PlanFailureView.swift
+//  AppFeatures
+//
+//  Created by abc on 3/18/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import AppUI
+import AppCore
+
+import SwiftData
+import SwiftUI
+
+struct PlanFailureView: View {
+    @EnvironmentObject var router: AppRouter
+    
+    @ObservedObject private var viewModel: ConsumptionMainViewModel
+    
+    @State private var isPresented: Bool = false
+    
+    init(viewModel: ConsumptionMainViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            Spacer()
+            
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 100))
+                .mask {
+                    LinearGradient(
+                        gradient: Gradient(colors: [.clear, .red.opacity(0.7)]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                }
+                .symbolEffect(.variableColor.cumulative, options: .repeat(3) ,isActive: isPresented)
+            
+            Text("😭Failure!")
+                .bold()
+            Text("You spent more than your budget!")
+            
+            Spacer()
+            
+            Button {
+                router.pop()
+            } label: {
+                Text("OK")
+            }
+            .buttonStyle(BottomButtonStyle())
+        } // VStack
+        .padding(20)
+        .background(Color.primary.colorInvert())
+        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+        .onAppear {
+            isPresented = true
+        }
+    }
+}
+
+#Preview {
+    @Previewable @StateObject var router = AppRouter()
+    PlanFailureView(viewModel: ConsumptionMainViewModel())
+        .environmentObject(router)
+        .modelContainer(previewContainer)
+}
