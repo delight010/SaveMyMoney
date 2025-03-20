2f4ccad8ce1e0310195800a917b0f726e3db6bdf
delight010 <osa010@knou.ac.kr>
2025-03-20 18:52:27 +0900

    Improve extension Color init

--- Projects/AppCore/Sources/Extension/Color+Extension.swift
+++ Projects/AppCore/Sources/Extension/Color+Extension.swift
@@ -11,10 +11,15 @@ import SwiftUI
 public extension Color {
     
     init(hex: String, opacity: Double = 1.0) {
-        let hex = Int(hex, radix: 16)!
-        let red = Double((hex >> 16) & 0xff) / 255
-        let green = Double((hex >> 8) & 0xff) / 255
-        let blue = Double((hex >> 0) & 0xff) / 255
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
         
         self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
     }
