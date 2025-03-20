8ab5b28f0828722838544fd0f9164b05043c7182
delight010 <osa010@knou.ac.kr>
2025-03-20 14:07:09 +0900

    Add preferredColorScheme modifier to RootView

--- Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
+++ Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
@@ -6,12 +6,15 @@ import SwiftUI
 
 @main
 struct SaveMyMoneyApp: App {
+    @Environment(\.colorScheme) private var colorScheme
+    @AppStorage("appColorScheme") private var appColorScheme: AppColorScheme = .system
     @StateObject private var router = AppRouter()
     
     var body: some Scene {
         WindowGroup {
             RootView(coordinator: RootCoordinator(router))
                 .environmentObject(router)
+                .preferredColorScheme(appColorScheme == .system ? nil : appColorScheme.colorScheme)
         }
         .modelContainer(for: Plan.self)
     }
