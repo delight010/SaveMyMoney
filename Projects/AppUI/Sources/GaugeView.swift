a168c6ef2ce046ec804700ab346e8bb7a3d6d911
delight010 <osa010@knou.ac.kr>
2025-03-20 19:32:53 +0900

    Add systemImage property in GaugeView

--- Projects/AppUI/Sources/GaugeView.swift
+++ Projects/AppUI/Sources/GaugeView.swift
@@ -14,33 +14,36 @@ public struct GaugeView: View {
     @State private var current: Double
     @State private var label: String = ""
     @State private var color: Color
+    @State private var systemImage: String
     
     @State private var min: Double = 0.0
     @State private var max: Double = 100.0
     
-    public init(current: Decimal, label: String, color: Color = .blue) {
+    public init(current: Decimal, label: String, color: Color = .blue, systemImage: String = "") {
         self.current = current.doubleValue
         self.label = label
         self.color = color
+        self.systemImage = systemImage
     }
     
-    public init(current: Double, label: String, color: Color = .blue) {
+    public init(current: Double, label: String, color: Color = .blue, systemImage: String = "") {
         self.current = current
         self.label = label
         self.color = color
+        self.systemImage = systemImage
     }
     
     public var body: some View {
         Gauge(value: current, in: min...max) {
-            Text(label)
+            Label(label, systemImage: systemImage)
         } currentValueLabel: {
             Text(String(format: "%.0f%%", current))
-        }
+        } // Gauge
         .gaugeStyle(.accessoryCircular)
         .tint(Gradient(colors: [color, Color.clear]))
     }
 }
 
 #Preview {
-    GaugeView(current: 20.0, label: "Food")
+    GaugeView(current: 20.0, label: "Food", systemImage: "fork.knife")
 }
