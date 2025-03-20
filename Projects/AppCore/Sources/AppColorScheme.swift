ca24f2c9de5b10d1b9e11efba18bec95770755d7
delight010 <osa010@knou.ac.kr>
2025-03-20 12:45:27 +0900

    Add String, CaseIterable protocol in AppColorScheme

---
+++ Projects/AppCore/Sources/AppColorScheme.swift
@@ -0,0 +1,15 @@
+//
+//  AppColorScheme.swift
+//  AppCore
+//
+//  Created by abc on 3/20/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import Foundation
+
+public enum AppColorScheme: String, CaseIterable {
+    case light
+    case dark
+    case system
+}
