9239f71ed3748f207e6a90caa2cb21a858c062af
delight010 <osa010@knou.ac.kr>
2025-03-23 00:06:18 +0900

    Create CircleAnimationView

---
+++ Projects/AppUI/Sources/CircleAnimationView.swift
@@ -0,0 +1,37 @@
+//
+//  CircleAnimationView.swift
+//  AppUI
+//
+//  Created by abc on 3/23/25.
+//  Copyright © 2025 delight010. All rights reserved.
+//
+
+import SwiftUI
+
+public struct CircleAnimationView: View {
+    @State private var isRotate: Bool = false
+    
+    public init() { }
+    
+    public var body: some View {
+        Circle()
+            .foregroundStyle(AngularGradient(colors: [.primaryColor.opacity(0), .primaryColor.opacity(0.3), .primaryColor.opacity(0.5)], center: .center, angle: .degrees(isRotate ? 360 : 0)))
+            .overlay {
+                Circle()
+                    .inset(by: 5)
+                    .fill(.background)
+            }
+            .onAppear {
+                withAnimation(
+                    Animation.linear(duration: 1.5)
+                        .repeatForever(autoreverses: false)
+                ) {
+                    isRotate.toggle()
+                }
+            }
+    }
+}
+
+#Preview {
+    CircleAnimationView()
+}
