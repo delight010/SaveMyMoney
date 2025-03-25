5b19b54c2fb0c2cf06bdd5ca23078af5b59de168
abc <>
2025-03-25 20:25:54 +0900

    Implement FirebaseCore in App

--- Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
+++ Projects/SaveMyMoney/Sources/SaveMyMoneyApp.swift
@@ -1,11 +1,24 @@
 import AppFeatures
 import AppCore
 
+import FirebaseCore
+
 import SwiftData
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
 @main
 struct SaveMyMoneyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     @Environment(\.colorScheme) private var colorScheme
     @AppStorage("appColorScheme") private var appColorScheme: AppColorScheme = .system
     @StateObject private var router = AppRouter()
