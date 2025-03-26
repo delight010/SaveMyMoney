84d24e19e4a98bb1eb4b2baffec5e170ea23c882
abc <>
2025-03-26 20:32:10 +0900

    Change Project baseSettings

--- Tuist/ProjectDescriptionHelpers/AppConfiguration.swift
+++ Tuist/ProjectDescriptionHelpers/AppConfiguration.swift
@@ -16,6 +16,9 @@ public enum AppConfiguration {
     public static let bundleIdentifier: String = "\(organizationName).\(appName.lowercased())"
     public static let sources: SourceFilesList = "Sources/**"
     public static let resources: ResourceFileElements = [.glob(pattern: "Resources/**")]
-    public static let baseSettings: SettingsDictionary = ["DEBUG_INFORMATION_FORMAT": "dwarf-with-dsym"]
+    public static let baseSettings: SettingsDictionary = [
+        "OTHER_LDFLAGS": "$(inherited) -ObjC",
+        "DEBUG_INFORMATION_FORMAT": "dwarf-with-dsym"
+    ]
     public static let debugConfig: ConfigurationName = "Debug"
 }
